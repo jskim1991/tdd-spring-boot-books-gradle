ed3fd01f833827f094c309c4c90e96ad0fd8ad92
Jaesik Kim <tensaijskim1991@gmail.com>
2021-04-12 21:10:13 +0900

    Adding integration tests

---
+++ src/integration-test/java/tddspringbootbooks/TddSpringBootBooksApplicationTests.java
@@ -0,0 +1,12 @@
+package tddspringbootbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSpringBootBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
